e05a3e81f20124b2527d04459eb43b1a84b5e123
OlawaleEmmanuel29 <157608193+OlawaleEmmanuel29@users.noreply.github.com>
2026-04-05 18:24:32 +0100

    Configure ESLint and TypeScript to ignore build errors
    Added ESLint and TypeScript configuration options to ignore errors during production builds.

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,16 @@ import type { NextConfig } from 'next';
 const nextConfig: NextConfig = {
   /* config options here */
   devIndicators: false,
+  eslint: {
+    // This will allow the production build to complete 
+    // even if there are formatting (Prettier) errors.
+    ignoreDuringBuilds: true,
+  },
+  typescript: {
+    // Optional: If you also hit Type errors, 
+    // you can ignore them with this setting:
+    ignoreBuildErrors: true,
+  },
 };
 
 export default nextConfig;
